803d116bf5d9ade5a375d40650cf056560ab1f6a
howlsmoving <ryan@DESKTOP-4RDRGBL.localdomain>
2022-02-09 19:37:55 -0600

    build

--- app/gem-farm/src/globals.ts
+++ app/gem-farm/src/globals.ts
@@ -3,9 +3,9 @@ import { PublicKey } from '@solana/web3.js';
 export const DEFAULTS = {
   CLUSTER: 'mainnet',
   GEM_BANK_PROG_ID: new PublicKey(
-    'bankHHdqMuaaST4qQk6mkzxGeKPHWmqdgor6Gs8r88m'
+    'BAnKk3APgi1ZaBPiwtaXz8sDktTgW1x2nRqcNsshH3u'
   ),
   GEM_FARM_PROG_ID: new PublicKey(
-    'farmL4xeBFVXJqtfxCzU9b28QACM7E2W2ctT6epAjvE'
+    'F181LCGQvSCgbsC9cCRmLBgQTNnwfrEHRjerYKNTjvyN'
   ),
 };
